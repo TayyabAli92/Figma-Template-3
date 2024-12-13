caed842617a35a30398130fdab67443424f98300
Tayyab Ali <hameshak110@gmail.com>
2024-12-14 00:18:09 +0500

    update

--- src/components/Men_Women_Card.tsx
+++ src/components/Men_Women_Card.tsx
@@ -1,24 +1,23 @@
 import Image from "next/image";
 
-
-interface menWomenCard{
+interface menWomenCard {
     image: string; 
     name: string;
     description: string;
     discount: string;
-};
+}
 
-export default function MenWomenCard({image, name, description, discount}:menWomenCard){
-    return(
-        <div className="flex flex-col gap-7 h-[400px] w-[300px]">
-            <Image src={image} alt={'showimage'} width={300} height={300} />
+export default function MenWomenCard({ image, name, description, discount }: menWomenCard) {
+    return (
+        <div className="flex flex-col gap-7 h-auto w-full max-w-[300px] mx-auto">
+            <Image src={image} alt="showimage" width={300} height={300} className="w-full h-auto" />
             <div>
                 <span className="flex justify-between w-full">
-                <h4>{name}</h4>
-                <p>{discount}</p>
+                    <h4 className="text-[10px] md:text-[15px] font-bold text-left">{name}</h4>
+                    <p className="text-[10px] md:text-[15px] font-bold text-right">{discount}</p>
                 </span>
-                <p className="text-[#757575]">{description}</p>
+                <p className="text-[#757575] text-sm md:text-base text-left">{description}</p>
             </div>
         </div>
-    )
+    );
 }
